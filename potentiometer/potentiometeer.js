4a7f6aefbedd36e27a3e2a598737f5df1ca5f1ff
denver daniels <denver@projectcodex.co>
2016-08-04 09:27:38 +0200

    another potentiometer added

--- potentiometer/potentiometeer.js
+++ potentiometer/potentiometeer.js
@@ -3,12 +3,16 @@ var http = require('http');
 var express = require('express'),
     app = module.exports.app = express(),
     io = require('socket.io'),
-     potentiometer;
+     potentiometer,potentiometer2;
 var board = new five.Board();
 app.use(express.static('public'));
 board.on('ready', function(){
   potentiometer = new five.Sensor({
     pin: 'A2',
+    freq: 250
+  });
+  potentiometer2 = new five.Sensor({
+    pin: 'A3',
     freq: 250
   });
 var led = new five.Led(11);
@@ -20,6 +24,14 @@ var led = new five.Led(11);
     console.log(list);
     led.brightness(list);
     io.emit('value', {
+        data: list
+    });
+  });
+  potentiometer2.on('data', function(){
+    var list = this.value;
+    console.log(list);
+    led.brightness(list);
+    io.emit('value1', {
         data: list
     });
   });
